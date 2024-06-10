2f2134aa6c468326654af6e8fb203e34e0548c44
Dawid Łuczak <dawid.luczak.it@gmail.com>
2024-06-10 10:58:45 +0200

    Add buttons

---
+++ projects/dlsd-angular-ui/src/lib/buttons/clear-button/clear-button.component.ts
@@ -0,0 +1,11 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'dlsd-clear-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './clear-button.component.html',
+  styleUrl: './clear-button.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class DLSDClearButtonComponent {}
