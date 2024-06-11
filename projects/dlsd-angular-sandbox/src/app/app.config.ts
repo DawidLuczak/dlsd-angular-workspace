52cd4309f54ce6a8f73a4a8ab1fed4680a84e43c
Dawid Łuczak <dawid.luczak.it@gmail.com>
2024-06-11 10:08:15 +0200

    Add translations

--- projects/dlsd-angular-sandbox/src/app/app.config.ts
+++ projects/dlsd-angular-sandbox/src/app/app.config.ts
@@ -1,17 +1,42 @@
+import { registerLocaleData } from '@angular/common';
+import { HttpBackend, provideHttpClient } from '@angular/common/http';
+import localePl from '@angular/common/locales/pl';
 import {
   ApplicationConfig,
+  LOCALE_ID,
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { MultiTranslateHttpLoader } from 'ngx-translate-multi-http-loader';
 import { routes } from './app.routes';
+
+registerLocaleData(localePl);
+
+export const createTranslateLoader = (http: HttpBackend) =>
+  new MultiTranslateHttpLoader(http, [
+    { prefix: '/assets/i18n/', suffix: '.json' },
+  ]);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    importProvidersFrom(TranslateModule.forRoot()),
+    provideHttpClient(),
+    importProvidersFrom(
+      TranslateModule.forRoot({
+        defaultLanguage: 'pl',
+        loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpBackend],
+        },
+      })
+    ),
+    {
+      provide: LOCALE_ID,
+      useValue: 'pl',
+    },
   ],
 };
