a73053f2295d13ea7f97efb1a93f3e33a014d13d
Dawid Łuczak <dawid.luczak.it@gmail.com>
2024-06-13 14:10:18 +0200

    Add form controls group

--- projects/dlsd-angular-sandbox/src/app/forms/forms.component.ts
+++ projects/dlsd-angular-sandbox/src/app/forms/forms.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import { ControlsComponent } from './controls/controls.component';
 import { InputsComponent } from './inputs/inputs.component';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [InputsComponent],
+  imports: [InputsComponent, ControlsComponent],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss',
 })
